b52ecbaacf4c8f29fa83ad9b0d30267239be4e42
Gessmass <martinweilldrums@gmail.com>
2023-09-07 00:01:10 +0200

    le planetes sont bientot finies wallah

--- frontend/src/components/SelectPlanet.jsx
+++ frontend/src/components/SelectPlanet.jsx
@@ -2,40 +2,48 @@ export default function SelectPlanet() {
   return (
     <div className="selectPlanetDiv">
       <div className="planetDiv">
+        <div className="divTextPlanet">
+          <p>Terra Nova</p>
+        </div>
         <div className="divCentrage">
           <div className="divPlanet">
             <div className="barre"></div>
             <div id="terraNova"></div>
           </div>
         </div>
-        <p>Terra Nova</p>
       </div>
       <div className="planetDiv">
+        <div className="divTextPlanet">
+          <p>Pyrotopia</p>
+        </div>
         <div className="divCentrage">
           <div className="divPlanet2">
             <div className="barre2"></div>
             <div id="Pyrotopia"></div>
           </div>
         </div>
-        <p>Pyrotopia</p>
       </div>
       <div className="planetDiv">
+        <div className="divTextPlanet">
+          <p>Glaciara</p>
+        </div>
         <div className="divCentrage">
           <div className="divPlanet3">
             <div className="barre3"></div>
             <div id="Glaciara"></div>
           </div>
         </div>
-        <p>Glaciara</p>
       </div>
       <div className="planetDiv">
+        <div className="divTextPlanet">
+          <p>Terraria</p>
+        </div>
         <div className="divCentrage">
           <div className="divPlanet4">
             <div className="barre4"></div>
             <div id="Terraria"></div>
           </div>
         </div>
-        <p>Terraria</p>
       </div>
     </div>
   )
