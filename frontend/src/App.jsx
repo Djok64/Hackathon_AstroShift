c1652d9b0168f0a68e80bb2f0db2b17ddfc38d58
HpJkR <m.hertereau@gmail.com>
2023-09-06 21:03:23 +0200

    create form planet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import Home from "./pages/Home"
-
 import "./App.css"
+import SystemeSolaire from "./components/SystemeSolaire"
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <SystemeSolaire />
     </div>
   )
 }
