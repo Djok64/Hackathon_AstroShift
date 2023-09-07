fda02d044bc4e0c2f9379e5bd3b3ad5f22a77d3e
Djok64 <joakym.anc@gmail.com>
2023-09-08 00:42:48 +0200

    making baskets for the article  , now a days the basket appair in a pop up but he's not functionnal we can' add somes products from the button add panier on the product card , we had to make the basket fonctions , and save the article selected  on the local storage and display them on the basket pop up

--- frontend/src/components/Basket.jsx
+++ frontend/src/components/Basket.jsx
@@ -1,45 +1,27 @@
 import { useContext } from "react"
 import myContext from "./useContext"
-import LogoCroix from "../assets/images/croix.png"
 
 export default function Basket() {
+  // On utilise le hook `useContext` pour accéder à notre contexte et déstructurer
+  // la propriété `setBasketPopup` pour pouvoir l'utiliser.
   const { setBasketPopup } = useContext(myContext)
-  // const [selectedProduct, setSelectedProduct] = useState("")
 
-  // const [productInfos, setProductInfos] = useState(null)
-
-  // useEffect(() => {
-  //   axios
-  //     .get(`${import.meta.env.VITE_BACKEND_URL}/survivalItems`)
-  //     .then((res) => {
-  //       setProductInfos(res.data).catch((err) => console.info(err))
-  //     })
-  // }, [])
-
-  // const displayPopup = (object) => {
-  //   setSelectedProduct(object)
-  //   setBasketPopup(true)
-  // }
-
-  // const closePopup = () => {
-  //   setSelectedProduct(null)
-  //   setBasketPopup(false)
-  // }
-
-  const closePopup = () => {
-    setBasketPopup(false)
-    // setSelectedProduct(null)
+  // On crée une fonction `handleOverlayClick` qui sera appelée chaque fois qu'on
+  // clique sur l'overlay. On vérifie si l'élément cliqué est l'overlay lui-même
+  // et, si c'est le cas, on ferme la pop-up.
+  const handleOverlayClick = (e) => {
+    if (e.target.id === "overlay") {
+      setBasketPopup(false)
+    }
   }
-
+  // On retourne notre composant JSX. L'overlay est la couche grise qui s'affiche
+  // derrière la pop-up et occupe toute la fenêtre.
   return (
-    <>
+    <div id="overlay" onClick={handleOverlayClick}>
       <div id="BasketGlobal">
-        <div className="croix" onClick={closePopup}>
-          <img src={LogoCroix} alt="Close" />
-        </div>
-        <p>je suis dans panier</p>
+        <p>je suis dans le panier</p>
         <button onClick={() => setBasketPopup(false)}>le test ultime</button>
       </div>
-    </>
+    </div>
   )
 }
